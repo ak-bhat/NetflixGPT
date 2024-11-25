d1e1a0292d5fe67ee93ecc720e686a247c22fa09
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-25 13:03:07 +0530

    Form validation done using useRef

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import checkValidData from "../utils/validate";
 
 const Login = () => {
   const [isSigninForm, setIsSignInForm] = useState(true);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef();
+  const password = useRef();
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message)
+  }
 
   const toggleSignInform = () => {
     setIsSignInForm(!isSigninForm);
@@ -16,7 +27,7 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className=" w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75">
+      <form onSubmit={(e)=>e.preventDefault()} className=" w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-75">
         <h1 className="font-bold text-3xl py-4">
           {isSigninForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -31,13 +42,16 @@ const Login = () => {
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
+          ref={email}
         />
         <input
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
+          ref={password}
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        <p className="text-red-600 font-bold text-lg">{errorMessage}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
           {isSigninForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-6">
