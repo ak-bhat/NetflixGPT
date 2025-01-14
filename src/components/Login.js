74a17fc145791b96641b953007ca55dd05a4200b
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2025-01-14 17:30:25 +0530

    Routes auth bug fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,6 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
@@ -16,7 +15,6 @@ const Login = () => {
 
   const [errorMessage, setErrorMessage] = useState(null);
 
-  const navigate = useNavigate()
 
   const dispatch = useDispatch()
 
@@ -53,8 +51,6 @@ const Login = () => {
                   photoURL:photoURL
               })
             )
-
-            navigate("/browse")
           })
           .catch((error)=>{
             setErrorMessage(error.message)
@@ -76,8 +72,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          navigate("/browse")
-          console.log(user);
           
         })
         .catch((error) => {
