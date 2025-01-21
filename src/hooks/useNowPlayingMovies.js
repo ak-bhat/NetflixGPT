0c15980b8931fa2bd6c97d106e34af257ff43601
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2025-01-21 16:57:07 +0530

    key updated

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux"
-import { API_URL, API_OPTIONS } from "../utils/constants"
+import { API_URL } from "../utils/constants"
 import { addNowPlayingMovies } from "../utils/moviesSlice"
 import { useEffect } from "react"
 
@@ -9,7 +9,7 @@ const useNowPlayingMovies = () =>{
     const dispatch = useDispatch()
 
     const getNowPlayingMovies = async () =>{  // API Req
-      const data = await fetch(API_URL, API_OPTIONS)
+      const data = await fetch(API_URL, process.env.REACT_APP_API_OPTIONS)
       const json = await data.json()
       console.log(json.results);
       dispatch(addNowPlayingMovies(json.results))
